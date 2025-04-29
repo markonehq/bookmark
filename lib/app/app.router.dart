01afd6a092975c8c5c9b6519d7feee951fc027d0
psantosh16 <santoshphadtare1610@gmail.com>
2025-04-29 21:14:54 +0530

    feat: Add Auth and Firestore services, update routing and dependency injection

--- lib/app/app.router.dart
+++ lib/app/app.router.dart
@@ -5,13 +5,14 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:bookmark/ui/views/auth/auth_view.dart' as _i5;
 import 'package:bookmark/ui/views/home/home_view.dart' as _i3;
 import 'package:bookmark/ui/views/onboarding/onboarding_view.dart' as _i4;
 import 'package:bookmark/ui/views/startup/startup_view.dart' as _i2;
-import 'package:flutter/material.dart' as _i5;
+import 'package:flutter/material.dart' as _i6;
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart' as _i1;
-import 'package:stacked_services/stacked_services.dart' as _i6;
+import 'package:stacked_services/stacked_services.dart' as _i7;
 
 class Routes {
   static const startupView = '/';
@@ -20,10 +21,13 @@ class Routes {
 
   static const onboardingView = '/onboarding-view';
 
+  static const authView = '/auth-view';
+
   static const all = <String>{
     startupView,
     homeView,
     onboardingView,
+    authView,
   };
 }
 
@@ -41,24 +45,34 @@ class StackedRouter extends _i1.RouterBase {
       Routes.onboardingView,
       page: _i4.OnboardingView,
     ),
+    _i1.RouteDef(
+      Routes.authView,
+      page: _i5.AuthView,
+    ),
   ];
 
   final _pagesMap = <Type, _i1.StackedRouteFactory>{
     _i2.StartupView: (data) {
-      return _i5.MaterialPageRoute<dynamic>(
+      return _i6.MaterialPageRoute<dynamic>(
         builder: (context) => const _i2.StartupView(),
         settings: data,
       );
     },
     _i3.HomeView: (data) {
-      return _i5.MaterialPageRoute<dynamic>(
+      return _i6.MaterialPageRoute<dynamic>(
         builder: (context) => const _i3.HomeView(),
         settings: data,
       );
     },
     _i4.OnboardingView: (data) {
-      return _i5.MaterialPageRoute<dynamic>(
+      return _i6.MaterialPageRoute<dynamic>(
         builder: (context) => const _i4.OnboardingView(),
+        settings: data,
+      );
+    },
+    _i5.AuthView: (data) {
+      return _i6.MaterialPageRoute<dynamic>(
+        builder: (context) => const _i5.AuthView(),
         settings: data,
       );
     },
@@ -71,7 +85,7 @@ class StackedRouter extends _i1.RouterBase {
   Map<Type, _i1.StackedRouteFactory> get pagesMap => _pagesMap;
 }
 
-extension NavigatorStateExtension on _i6.NavigationService {
+extension NavigatorStateExtension on _i7.NavigationService {
   Future<dynamic> navigateToStartupView([
     int? routerId,
     bool preventDuplicates = true,
@@ -114,6 +128,20 @@ extension NavigatorStateExtension on _i6.NavigationService {
         transition: transition);
   }
 
+  Future<dynamic> navigateToAuthView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return navigateTo<dynamic>(Routes.authView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
   Future<dynamic> replaceWithStartupView([
     int? routerId,
     bool preventDuplicates = true,
@@ -150,6 +178,20 @@ extension NavigatorStateExtension on _i6.NavigationService {
         transition,
   ]) async {
     return replaceWith<dynamic>(Routes.onboardingView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithAuthView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return replaceWith<dynamic>(Routes.authView,
         id: routerId,
         preventDuplicates: preventDuplicates,
         parameters: parameters,
