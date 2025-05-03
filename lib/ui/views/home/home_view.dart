1488614662d597602dbfffa4510f390ed57ba45a
psantosh16 <santoshphadtare1610@gmail.com>
2025-05-03 18:55:14 +0530

    fix: adjust padding in settings sheet and home view

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -22,198 +22,196 @@ class HomeView extends StatelessWidget {
     return ViewModelBuilder<HomeViewModel>.reactive(
       onViewModelReady: (viewModel) => viewModel.init(),
       viewModelBuilder: () => HomeViewModel(),
-      builder: (context, viewModel, child) => SafeArea(
-        child: Scaffold(
-          extendBodyBehindAppBar: true,
-          floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-          floatingActionButton: SpeedDial(
-            animatedIcon: AnimatedIcons.menu_close,
-            backgroundColor: context.colorScheme.black,
-            childrenButtonSize: Size(50.w, 60.h),
-            foregroundColor: context.colorScheme.white,
-            overlayColor: Colors.black,
-            overlayOpacity: 0.5,
-            spacing: 10,
-            spaceBetweenChildren: 5,
-            children: [
-              SpeedDialChild(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(100.r),
-                ),
-                child: const Icon(Icons.insert_drive_file),
-                backgroundColor: context.colorScheme.black,
-                foregroundColor: context.colorScheme.white,
-                // label: 'Files',
-                onTap: () {
-                  viewModel.handleFileAction();
-                },
+      builder: (context, viewModel, child) => Scaffold(
+        extendBodyBehindAppBar: true,
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        floatingActionButton: SpeedDial(
+          animatedIcon: AnimatedIcons.menu_close,
+          backgroundColor: context.colorScheme.black,
+          childrenButtonSize: Size(50.w, 60.h),
+          foregroundColor: context.colorScheme.white,
+          overlayColor: Colors.black,
+          overlayOpacity: 0.5,
+          spacing: 10,
+          spaceBetweenChildren: 5,
+          children: [
+            SpeedDialChild(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100.r),
               ),
-              SpeedDialChild(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(100.r),
-                ),
-                child: const Icon(Icons.link),
-                backgroundColor: context.colorScheme.black,
-                foregroundColor: context.colorScheme.white,
-                // label: 'Links',
-                onTap: () {
-                  viewModel.handleLinkAction();
-                },
-              ),
-              SpeedDialChild(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(100.r),
-                ),
-                child: const Icon(Icons.note),
-                backgroundColor: context.colorScheme.black,
-                foregroundColor: context.colorScheme.white,
-                // label: 'Notes',
-                onTap: () {
-                  viewModel.handleNoteAction();
-                },
-              ),
-              SpeedDialChild(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(100.r),
-                ),
-                child: const Icon(Icons.picture_as_pdf),
-                backgroundColor: context.colorScheme.black,
-                foregroundColor: context.colorScheme.white,
-                // label: 'PDFs',
-                onTap: () {
-                  viewModel.handlePdfAction();
-                },
-              ),
-            ],
-          ),
-          appBar: AppBar(
-            surfaceTintColor: Colors.transparent,
-            automaticallyImplyLeading: false,
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            leading: Padding(
-              padding: EdgeInsets.only(left: 16.w),
-              child: GestureDetector(
-                  onTap: () {
-                    viewModel.showSettingBottomSheet();
-                  },
-                  child: CircleAvatar(
-                    backgroundImage: NetworkImage(viewModel.avatar),
-                    radius: 20.w,
-                    backgroundColor: Colors.grey[300],
-                  )),
+              child: const Icon(Icons.insert_drive_file),
+              backgroundColor: context.colorScheme.black,
+              foregroundColor: context.colorScheme.white,
+              // label: 'Files',
+              onTap: () {
+                viewModel.handleFileAction();
+              },
             ),
-            title: AnimatedContainer(
-              padding: EdgeInsets.only(left: 10.w),
-              duration: const Duration(milliseconds: 300),
-              alignment: viewModel._isSearching
-                  ? Alignment.centerRight
-                  : Alignment.centerLeft,
-              width: viewModel._isSearching
-                  ? MediaQuery.of(context).size.width - 90.w
-                  : 0,
-              curve: Curves.easeInOut,
-              child: viewModel._isSearching
-                  ? TextField(
-                      controller: viewModel._searchController,
-                      autofocus: true,
-                      onSubmitted: (String value) {
-                        viewModel.showSearchBottomSheet();
-                      },
-                      cursorColor: Colors.orangeAccent[200],
-                      onChanged: viewModel.updateSearchQuery,
-                      decoration: InputDecoration(
-                        hintText: "Search...",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(100.w),
-                          borderSide: BorderSide.none,
-                        ),
-                        filled: true,
-                        fillColor: Colors.grey[200],
-                        prefixIcon: const Icon(Icons.search),
-                        suffixIcon: IconButton(
-                          icon: const Icon(Icons.close),
-                          onPressed: () {
-                            viewModel.clearSearch();
-                          },
-                        ),
-                      ))
-                  : null,
+            SpeedDialChild(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100.r),
+              ),
+              child: const Icon(Icons.link),
+              backgroundColor: context.colorScheme.black,
+              foregroundColor: context.colorScheme.white,
+              // label: 'Links',
+              onTap: () {
+                viewModel.handleLinkAction();
+              },
             ),
-            actions: [
-              if (!viewModel._isSearching)
-                IconButton(
-                  padding: EdgeInsets.only(right: 16.w),
-                  icon: const Icon(Icons.search),
-                  onPressed: () {
-                    viewModel.navigateToSearch();
-                    // viewModel._isSearching = true;
-                    // viewModel.notifyListeners();
-                  },
-                ),
-            ],
+            SpeedDialChild(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100.r),
+              ),
+              child: const Icon(Icons.note),
+              backgroundColor: context.colorScheme.black,
+              foregroundColor: context.colorScheme.white,
+              // label: 'Notes',
+              onTap: () {
+                viewModel.handleNoteAction();
+              },
+            ),
+            SpeedDialChild(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100.r),
+              ),
+              child: const Icon(Icons.picture_as_pdf),
+              backgroundColor: context.colorScheme.black,
+              foregroundColor: context.colorScheme.white,
+              // label: 'PDFs',
+              onTap: () {
+                viewModel.handlePdfAction();
+              },
+            ),
+          ],
+        ),
+        appBar: AppBar(
+          surfaceTintColor: Colors.transparent,
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: Padding(
+            padding: EdgeInsets.only(left: 16.w),
+            child: GestureDetector(
+                onTap: () {
+                  viewModel.showSettingBottomSheet();
+                },
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(viewModel.avatar),
+                  radius: 20.w,
+                  backgroundColor: Colors.grey[300],
+                )),
           ),
-          body: !viewModel.isBusy
-              ? Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [
-                        Colors.yellow[400]!,
-                        Colors.yellow[100]!,
-                        Colors.yellow[100]!,
-                        Colors.yellow[50]!,
-                        Colors.white,
-                        Colors.white,
-                        Colors.white,
-                      ],
-                    ).withOpacity(0.5),
-                  ),
-                  child: GestureDetector(
-                    onTap: () {
-                      FocusScope.of(context).unfocus();
+          title: AnimatedContainer(
+            padding: EdgeInsets.only(left: 10.w),
+            duration: const Duration(milliseconds: 300),
+            alignment: viewModel._isSearching
+                ? Alignment.centerRight
+                : Alignment.centerLeft,
+            width: viewModel._isSearching
+                ? MediaQuery.of(context).size.width - 90.w
+                : 0,
+            curve: Curves.easeInOut,
+            child: viewModel._isSearching
+                ? TextField(
+                    controller: viewModel._searchController,
+                    autofocus: true,
+                    onSubmitted: (String value) {
+                      viewModel.showSearchBottomSheet();
                     },
-                    child: AnimatedSwitcher(
-                      duration: const Duration(milliseconds: 400),
-                      child: ScreenWrapper(
-                        child: CustomScrollView(
-                          keyboardDismissBehavior:
-                              ScrollViewKeyboardDismissBehavior.onDrag,
-                          slivers: [
-                            SliverPadding(
-                              padding: EdgeInsets.symmetric(vertical: 16.h),
-                              sliver: SliverMasonryGrid.count(
-                                crossAxisCount: 2,
-                                itemBuilder: (context, index) {
-                                  final link = viewModel.bookmarks[index].link;
-                                  final bookmark =
-                                      HomeViewModel.ogDataCache[link] ??
-                                          viewModel.bookmarks[index];
+                    cursorColor: Colors.orangeAccent[200],
+                    onChanged: viewModel.updateSearchQuery,
+                    decoration: InputDecoration(
+                      hintText: "Search...",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(100.w),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      prefixIcon: const Icon(Icons.search),
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.close),
+                        onPressed: () {
+                          viewModel.clearSearch();
+                        },
+                      ),
+                    ))
+                : null,
+          ),
+          actions: [
+            if (!viewModel._isSearching)
+              IconButton(
+                padding: EdgeInsets.only(right: 16.w),
+                icon: const Icon(Icons.search),
+                onPressed: () {
+                  viewModel.navigateToSearch();
+                  // viewModel._isSearching = true;
+                  // viewModel.notifyListeners();
+                },
+              ),
+          ],
+        ),
+        body: !viewModel.isBusy
+            ? Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Colors.yellow[400]!,
+                      Colors.yellow[100]!,
+                      Colors.yellow[100]!,
+                      Colors.yellow[50]!,
+                      Colors.white,
+                      Colors.white,
+                      Colors.white,
+                    ],
+                  ).withOpacity(0.5),
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    FocusScope.of(context).unfocus();
+                  },
+                  child: AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 400),
+                    child: ScreenWrapper(
+                      child: CustomScrollView(
+                        keyboardDismissBehavior:
+                            ScrollViewKeyboardDismissBehavior.onDrag,
+                        slivers: [
+                          SliverPadding(
+                            padding: EdgeInsets.symmetric(vertical: 16.h),
+                            sliver: SliverMasonryGrid.count(
+                              crossAxisCount: 2,
+                              itemBuilder: (context, index) {
+                                final link = viewModel.bookmarks[index].link;
+                                final bookmark =
+                                    HomeViewModel.ogDataCache[link] ??
+                                        viewModel.bookmarks[index];
 
-                                  return buildBookmarkCard(
-                                          data: bookmark,
-                                          font: font,
-                                          context: context)
-                                      .animate()
-                                      .fadeIn(
-                                          duration: 400.ms,
-                                          delay: (index * 100).ms)
-                                      .slide(begin: const Offset(0, 0.1));
-                                },
-                                childCount: viewModel.bookmarks.length,
-                                mainAxisSpacing: 4.h,
-                                crossAxisSpacing: 4.w,
-                              ),
+                                return buildBookmarkCard(
+                                        data: bookmark,
+                                        font: font,
+                                        context: context)
+                                    .animate()
+                                    .fadeIn(
+                                        duration: 400.ms,
+                                        delay: (index * 100).ms)
+                                    .slide(begin: const Offset(0, 0.1));
+                              },
+                              childCount: viewModel.bookmarks.length,
+                              mainAxisSpacing: 4.h,
+                              crossAxisSpacing: 4.w,
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                )
-              : loadingCircular(),
-        ),
+                ),
+              )
+            : loadingCircular(),
       ),
     );
   }
