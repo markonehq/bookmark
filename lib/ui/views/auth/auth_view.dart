ec044980b6aca878e372147fcfc061ea19c3955d
Niranjan Dorage <niranjandorage@gmail.com>
2025-05-03 23:40:44 +0530

    feat: login get started UI

--- lib/ui/views/auth/auth_view.dart
+++ lib/ui/views/auth/auth_view.dart
@@ -2,7 +2,6 @@ import 'package:bookmark/services/auth_service.dart' show AuthService;
 import 'package:bookmark/ui/common_widgets/loading_circular.dart';
 import 'package:bookmark/utils/assets_constants.dart';
 import 'package:bookmark/utils/file_exporter.dart';
-import 'package:flutter_svg/svg.dart';
 
 part 'auth_viewmodel.dart';
 
@@ -16,28 +15,79 @@ class AuthView extends StatelessWidget {
       onViewModelReady: (model) => model.init(),
       builder: (context, model, child) => Scaffold(
         backgroundColor: context.colorScheme.scaffoldColor,
-        body: Center(
-          child: SizedBox(
-            width: 200.w,
-            child: ElevatedButton(
-                onPressed: () {
-                  model.login();
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset(
-                      AssetsConstants.google,
-                      height: 24.h,
-                      width: 24.w,
+        body: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 100.w,
+                    child: const Image(
+                        image: AssetImage(AssetsConstants.logoClrBg)),
+                  ),
+                  20.verticalSpace,
+                  Text(
+                    "Save It Once. Find It Fast. Know More",
+                    textAlign: TextAlign.center,
+                    style: context.textTheme.bodyLarge?.copyWith(
+                      color: context.colorScheme.black,
+                      fontWeight: FontWeight.w500,
                     ),
-                    8.horizontalSpace,
-                    const Text("Google Sign In"),
-                  ],
-                )),
-          ),
+                  ),
+                  4.verticalSpace,
+                  Text(
+                    "Smarter, Faster, AI-Powered.",
+                    textAlign: TextAlign.center,
+                    style: context.textTheme.bodyLarge?.copyWith(
+                      color: context.colorScheme.black,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 20.h,
+              left: 16.w,
+              right: 16.w,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: const Color(0xFFF2C600),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.r),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 10.h),
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Get Started",
+                  style: context.textTheme.titleLarge?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+// ElevatedButton(
+//                 onPressed: () {
+//                   model.login();
+//                 },
+//                 child: Row(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: [
+//                     SvgPicture.asset(
+//                       AssetsConstants.google,
+//                       height: 24.h,
+//                       width: 24.w,
+//                     ),
+//                     8.horizontalSpace,
+//                     const Text("Google Sign In"),
+//                   ],
+//                 )),
